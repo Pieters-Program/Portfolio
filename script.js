321f45f806f36f4b2cc2067669dac6187259494f
Pieters-Program <maxbezemer@hotmail.nl>
2023-11-07 16:30:23 +0100

    2 new projects

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+var paragraphs = document.getElementsByTagName('p');
+for (var i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].classList.toggle("dark-mode-paragraph");
+}
+
+var experienceElement = document.getElementById('experience');
+ experienceElement.classList.toggle("dark-mode-experience");
+}
+
